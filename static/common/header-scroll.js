9b5d3c19b6583b615febf221dad11d6a98dd3916
Eric Scouten <eric@scouten.com>
2021-05-03 16:46:06 -0700

    Ooh, this header structure is getting diggity. (Grin.)

---
+++ static/common/header-scroll.js
@@ -0,0 +1,18 @@
+function headerScroll() {
+    const distanceY = window.pageYOffset || document.documentElement.scrollTop;
+    const miniHeader = document.getElementById('header');
+    
+    if (distanceY > 180) {
+        miniHeader.classList.add("show-title");
+    } else {
+        miniHeader.classList.remove("show-title");
+    }
+
+    if (distanceY > 20) {
+        miniHeader.classList.add("hide-name");
+    } else {
+        miniHeader.classList.remove("hide-name");
+    }
+}
+  
+window.addEventListener('scroll', headerScroll);
