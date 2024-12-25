769454666d6897220e920a6c70e02ad9c84f9c66
Eric Scouten <git@scouten.me>
2024-12-25 11:14:17 -0800

    Add support for CDN-based GPX markers

--- static/common/add-gps-markers.js
+++ static/common/add-gps-markers.js
@@ -53,6 +53,42 @@ function addGpxMarker(map, id, lat, lon) {
     return marker;
 }
 
+function addCdnGpxMarker(map, id, lat, lon, cdn_key) {
+    const contentString =
+        '<div class="gpx-marker">' +
+        '<a href="#' + id + '"><img src="https://img.ericscouten.com/' + cdn_key + '/' + id + '-400w.jpg" alt=""></a>' +
+        '</div>';
+
+    const infoWindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 200,
+    });
+
+    const marker = new google.maps.Marker({
+        position: { lat: lat, lng: lon },
+        map,
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 7,
+            fillOpacity: 1,
+            strokeWeight: 2,
+            fillColor: '#36b3b3',
+            strokeColor: '#ffffff',
+        },
+        title: "{{id}}",
+    });
+
+    marker.addListener("click", () => {
+        openInfoWindow(map, infoWindow, marker);
+    });
+    
+    marker.addListener("mouseover", () => {
+        openInfoWindow(map, infoWindow, marker);
+    });
+
+    return marker;
+}
+
 function addParkMarker(map, seq, name, slug, lat, lon, cover) {
     const contentString =
         '<div class="park-marker">' +
